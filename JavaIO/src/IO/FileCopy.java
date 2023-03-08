d6dee244451cf057253c7c6966d2805345f8482e
pparkjs <qkrwjdtn741@naver.com>
2023-03-08 11:30:51 +0900

    jpg 파일을 복사하기(수정)

--- JavaIO/src/IO/FileCopy.java
+++ JavaIO/src/IO/FileCopy.java
@@ -1,73 +1,82 @@
 package IO;
 
+import java.io.BufferedInputStream;
+import java.io.BufferedOutputStream;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.io.IOException;
 
 /*
- 	'd:/d_other' 폴더에 있는 '펭귄.jpg' 파일을 
- 	'd:/d_other/연습용' 폴더에 '복사본_펭귄.jpg'파일로 복사하는 프로그램을 작성하시오.
- */
+	문제) 'd:/d_other'폴더에 있는 '펭귄.jpg'파일을
+		  'd:/d_other/연습용'폴더에 '복사본_펭귄.jpg'파일로 복사하는 프로그램을 작성하시오.
+*/
+
 public class FileCopy {
+
 	public static void main(String[] args) {
-
-		//			FileInputStream fin = null;
-		//			FileOutputStream fout = null;
-		//			try {
-		//				fin = new FileInputStream("d:/d_other/펭귄.jpg");
-		//				fout = new FileOutputStream("d:/d_other/복사본_펭귄.jpg");
-		//				
-		//				int c; // 읽어온 데이터를 저장할 변수
-		//				while((c=fin.read()) != -1) {
-		//					// 읽어온 데이터를 복사본_펭귄.jpg에 담아준다.
-		//					fout.write(c);
-		//				}
-		//				
-		//			} catch (IOException e) {
-		//				// TODO: handle exception
-		//			}finally {
-		//				//스트림 닫기
-		//				if(fin!=null) try { fin.close();}catch(IOException e){}
-		//				if(fout!=null) try { fout.close();}catch(IOException e){}
-		//				
-
-		//			}
-
 		File file = new File("d:/d_other/펭귄.jpg");
-		if(!file.exists()) { // 원본파일이 없으면...
-			System.out.println("복사할 원본 파일 " +file.getName() + " 이(가) 없습니다.");
-			System.out.println("복사할 작업을 중지합니다..");
+		
+		if(!file.exists()) {  //  원본 파일이 없으면...
+			System.out.println("복사할 원본 파일 " + file.getName() + " 이(가) 없습니다.");
+			System.out.println("복사 작업을 중지합니다...");
 			return;
 		}
-
+		
+		File targetDir = new File("d:/d_other/연습용");
+		if(!targetDir.exists()) {  // 저장할 폴더가 없으면...
+			targetDir.mkdirs();
+		}
+		
+		File targetFile = new File(targetDir, "복사본_펭귄.jpg");
+		
+		
 		FileInputStream fin = null;
 		FileOutputStream fout = null;
-
+		
+		BufferedInputStream bin = null;
+		BufferedOutputStream bout = null;
 		try {
-			//원본 파일을 처리할 스트림 객체 생성
+			// 원본 파일을 처리할 스트림 객체 생성
 			fin = new FileInputStream(file);
-
-			//대상 파일을 처리할 스트림 객체 생성
-			fout = new FileOutputStream("d:/d_other/복사본_펭귄.jpg");
-
+			bin = new BufferedInputStream(fin);
+			
+			// 대상 파일을 처리할 스트림 객체 생성
+			fout = new FileOutputStream(targetFile);
+			bout = new BufferedOutputStream(fout);
+			
 			System.out.println("복사 시작...");
 			
-			int data;
-				
-			while((data = fin.read()) != -1) {
-				// 읽어온 데이터를 복사본_펭귄.jpg에 담아준다.
-				fout.write(data);
-			}
-			fout.flush();
+			int data; 	// 읽어온 데이터를 저장할 변수
 			
-			System.out.println("복사 완료....");
+//			while( (data = fin.read() ) != -1) {
+//				fout.write(data);
+//			}
+//			fout.flush();
+			
+			while( (data = bin.read() ) != -1) {
+				bout.write(data);
+			}
+			bout.flush();
+			
+			System.out.println("복사 완료...");
+			
 		} catch (IOException e) {
-
-		}finally {
-			//스트림 닫기
-			if(fin!=null) try { fin.close();}catch(IOException e){}
-			if(fout!=null) try { fout.close();}catch(IOException e){}
+			e.printStackTrace();
+		} finally {
+			
+			// 사용했던 스트림 닫기
+//			if(fin!=null) try { fin.close(); }catch(IOException e) {}
+//			if(fout!=null) try { fout.close(); }catch(IOException e) {}
+			
+			if(bin!=null) try { bin.close(); }catch(IOException e) {}
+			if(bout!=null) try { bout.close(); }catch(IOException e) {}
 		}
+		
+		
+		
+
 	}
+
 }
+
