2ef47cd1b0e6d37c6d8bdee3a0c5ff844c088a42
pparkjs <qkrwjdtn741@naver.com>
2023-03-10 11:24:15 +0900

    서버와 연결하여 메시지를 주고받을 클라이언트

---
+++ Network/src/tcp/TCPClient02.java
@@ -0,0 +1,21 @@
+package tcp;
+
+import java.net.Socket;
+
+public class TCPClient02 {
+	public static void main(String[] args) {
+		try {
+			Socket socket = new Socket("localhost", 7777);
+			System.out.println("서버에 연결되었습니다..");
+			
+			Sender sender = new Sender(socket);
+			Receiver receiver = new Receiver(socket);
+			
+			sender.start();
+			receiver.start();
+			
+		} catch (Exception e) {
+			// TODO: handle exception
+		}
+	}
+}
