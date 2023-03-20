c6f42346e3174022191a39bc6dd7ebc1cc66fe36
pparkjs <qkrwjdtn741@naver.com>
2023-03-20 12:42:26 +0900

    singleton study

---
+++ JDBC/src/singleton/SingletonTest.java
@@ -0,0 +1,20 @@
+package singleton;
+
+public class SingletonTest {
+	public static void main(String[] args) {
+//		MySingleton test1 = new MySingleton(); // 외부에서 new 명령으로 생성 불가
+		
+		// 아무리 많이 불러와도 한 번만 생성된다.
+		MySingleton test2 = MySingleton.getInstance();
+		MySingleton test3 = MySingleton.getInstance();
+		
+		System.out.println("test2 ==> " + test2.toString());
+		System.out.println("test3 ==> " + test3);
+		
+		System.out.println(test2 == test3);
+		System.out.println(test2.equals(test3));
+		
+		test2.displayTest();
+	}
+}
+
