208919fa9c92214af00104c3fd05b3bdf04188bc
pparkjs <qkrwjdtn741@naver.com>
2023-03-22 09:03:30 +0900

    Lprod테이블의 VO

---
+++ mybatisTest/src/basic/LprodVO.java
@@ -0,0 +1,32 @@
+package basic;
+
+public class LprodVO {
+	private int lprod_id;
+	private String lprod_gu;
+	private String lprod_nm;
+	
+	public int getLprod_id() {
+		return lprod_id;
+	}
+	public void setLprod_id(int lprod_id) {
+		this.lprod_id = lprod_id;
+	}
+	public String getLprod_gu() {
+		return lprod_gu;
+	}
+	public void setLprod_gu(String lprod_gu) {
+		this.lprod_gu = lprod_gu;
+	}
+	public String getLprod_nm() {
+		return lprod_nm;
+	}
+	public void setLprod_nm(String lprod_nm) {
+		this.lprod_nm = lprod_nm;
+	}
+	@Override
+	public String toString() {
+		return String.format("LprodVO [lprod_id=%s, lprod_gu=%s, lprod_nm=%s]", lprod_id, lprod_gu, lprod_nm);
+	}
+	
+	
+}
